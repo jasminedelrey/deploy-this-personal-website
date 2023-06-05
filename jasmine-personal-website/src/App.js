ac8d91f4d984e9f7d14ee54fc560c872c82c19a6
Jasmine Reyes <jasminereyess44@gmail.com>
2023-06-04 21:53:56 -0700

    add picture and bio

--- jasmine-personal-website/src/App.js
+++ jasmine-personal-website/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import profile from './jasminepic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <span className="photo-and-bio">
+        <img src={profile} className="App-logo" alt="logo" />
+        <div className= "biography">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey! My name is Jasmine Reyes.
         </p>
+        <p> I am a human that likes to code, sing, and watch movies! I currently work as a software engineer at Esri, with an expertise in frontend
+        software development. In my free time, I like to take long walks on the beach and watch sunsets. I am a budding boba connoisseur and 
+        a human bean figuring out her 20s in the most fearless way she can. </p>
+        <p>Let's be friends!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://drive.google.com/file/d/1DxNef13MBBiaPX1cc33wm12lMZU9AJc8/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Stalk my work experience here
         </a>
+        </div>
+        </span>
       </header>
     </div>
   );
